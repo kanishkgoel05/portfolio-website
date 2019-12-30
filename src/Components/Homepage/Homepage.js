c3bae1d5ba70ab98976378499bd3a80343fce68c
kanishkgoel05 <kanishkgoel059@gmail.com>
2019-12-30 15:58:07 +0530

    Video added

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import './homepage.css';
+import Fade from 'react-reveal/Fade';
+import Zoom from 'react-reveal/Zoom';
+import sample from '../../Utils/video.mp4'
 
 const homepage = () => {
     return (
         <div className="banner-start">
+            <video className='videoTag' autoPlay loop muted>
+                <source src={sample} type='video/mp4' />
+            </video>
+            <div className="box2">
+                <div className="box-name">
+                    <Fade left>
+                        Hi I'M Kanishk Goel
+                    </Fade>
+                </div>
+                <div>
+                    <Fade right delay={500}>
+                        Front-end Developer | Back-end Developer
+                    </Fade>
+                </div>
+            </div>
             <div className="box">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <div className="content">
-                    <h1>L2 Support Engineer OYO</h1>
-                    <h3>JANURAY 2019 - PRESENT</h3>
+                <div >
+                    <p><Zoom delay={700}>Love to learn new skills and make 100% efforts to put that on screen</Zoom></p>
+                    <p><Zoom delay={1200}>Want to work in new task which not only upskill me but also benefits others</Zoom></p>
                 </div>
             </div>
         </div>
---
+++ src/Components/Skills/Techinical Skills/technicalSkills.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TechnicalSkills = () => {
+    return (
+        <div>
+            <div>
+                Techincal Skills
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default TechnicalSkills;
